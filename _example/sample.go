6fcf0043ce14daa8c54ebe4d5f73448fb8f18fac
buchy <y.tabuchi12@gmail.com>
2018-04-12 21:56:24 +0900

    fix sample

--- _example/sample.go
+++ _example/sample.go
@@ -19,6 +19,18 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+	image2ascii.SetConf([]image2ascii.LumAsciiMap{
+		{0.0, 'a'},
+		{0.1, 'b'},
+		{0.2, 'c'},
+		{0.3, 'd'},
+		{0.4, 'e'},
+		{0.5, 'f'},
+		{0.6, 'g'},
+		{0.7, 'h'},
+		{0.8, 'i'},
+		{0.9, 'j'},
+		{1.0, 'k'},
+	})
 	fmt.Println(image2ascii.Convert(img))
 }
